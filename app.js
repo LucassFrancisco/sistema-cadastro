a2d7fa12c242ac4bf1e4dd6d5c32d50691d48de7
Lucas Francisco <lucas.francisco.santos05@escola.pr.gov.br>
2025-11-23 14:39:59 -0300

    qqq

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
 console. log ("rodando sistema de cadastro")
 console. log ("A aplicacao esta se conectando ao banco de dados")
-console. log("0 paulo fez esse commit sem voce ver na aula!")
-console. log("Enviando para o GitHub")
